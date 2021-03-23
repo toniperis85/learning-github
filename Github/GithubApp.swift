a4e828d4e923b000ca04cb3cc7c0aac68c21d3d1
Antoni Peris <toniperis85@gmail.com>
2021-03-22 22:10:33 -0500

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Antoni Peris on 3/22/21.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
